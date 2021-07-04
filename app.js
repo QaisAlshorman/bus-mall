c0ca929846e20ecacf6a05b087b811a319ee9dca
Qais Alshorman <qaishorman20@gmail.com>
2021-07-04 17:18:14 +0300

    the second try

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-//'use strict';
-
-const leftImageElement = document.getElementById('left-image');
-const middleImageElemnt = document.getElementById('midlle-image');
-const rightImageElement = document.getElementById('right-image');
+'use strict';
+let leftImageElement = document.getElementById('left-image');
+let middleImageElemnt = document.getElementById('midlle-image');
+let rightImageElement = document.getElementById('right-image');
 /// If we wanna to select the section itself
-// we modified it like const section1=ducument.getElementById('sec-one')
+let section1 = document.getElementById('sec-one');
 
-
-
-const maxAttempts = 5; // should modified to 25
+let maxAttempts = 25;
 // max = counter stop the event !
 let counter = 0;
 // constructor to declear the name and the path  # of votes the time shown
@@ -19,7 +16,6 @@ function Proudect(name, source) {
   this.shown = 0;
   Proudect.globArr.push(this);
 }
-
 Proudect.globArr = [];
 new Proudect('pen', 'img/pen.jpg');
 new Proudect('pet-sweep', 'img/pet-sweep.jpg');
@@ -41,77 +37,76 @@ new Proudect('dog-duck', 'img/dog-duck.jpg');
 new Proudect('dragon', 'img/dragon.jpg');
 new Proudect('wine-glass', 'img/wine-glass.jpg');
 
-
 // indexies of the imagies
 
-let leftIndex;
-let middleIndex;
-let rightIndex;
-
+let leftIndex = null;
+let middleIndex = null;
+let rightIndex = null;
 function renderImages() {
   leftIndex = generateRandomIndex();
   middleIndex = generateRandomIndex();
   rightIndex = generateRandomIndex();
-
-
-  while (leftIndex === rightIndex || leftIndex === middleIndex || middleIndex=== rightIndex) {
+  while (leftIndex === rightIndex || leftIndex === middleIndex || middleIndex === rightIndex) {
     leftIndex = generateRandomIndex();
-    middleIndex=generateRandomIndex();
+    middleIndex = generateRandomIndex();
   }
 
-  // console.log('After',leftIndex);
-  // console.log('After',rightIndex);
-  leftImageElement.src = Proudect.globArr[leftIndex].source;
-  middleImageElemnt.scr = Proudect.globArr[middleIndex].source;
-  rightImageElement.src = Proudect.globArr[rightIndex].source;
 
+  leftImageElement.src = Proudect.globArr[leftIndex].source;
+  Proudect.globArr[leftIndex].shown++;
+
+  middleImageElemnt.src = Proudect.globArr[middleIndex].source;
+  Proudect.globArr[middleIndex].shown++;
+
+  rightImageElement.src = Proudect.globArr[rightIndex].source;
+  Proudect.globArr[rightIndex].shown++;
 
 }
-
 renderImages();
-
 
 // adding event listner to the imeges seperatlly
 
 leftImageElement.addEventListener('click', handleClick);
 middleImageElemnt.addEventListener('click', handleClick);
 rightImageElement.addEventListener('click', handleClick);
+section1.addEventListener('click', handleClick);
 
 function handleClick(event) {
   counter++;
-
   if (maxAttempts >= counter) {
     if (event.target.id === 'left-image') {
       Proudect.globArr[leftIndex].votes++;
+
     } else if (event.target.id === 'right-image') {
       Proudect.globArr[rightIndex].votes++;
+
+
     }
-    else if (event.target.id === 'middle-image') {
+    else if (event.target.id === 'midlle-image') {
       Proudect.globArr[middleIndex].votes++;
+
     }
     renderImages();
   } else {
     renderList();
   }
-}
 
+
+}
 
 function renderList() {
   const ul = document.getElementById('unList');
+  let li;
   for (let i = 0; i < Proudect.globArr.length; i++) {
-    let li = document.createElement('li');
+    li = document.createElement('li');
     ul.appendChild(li);
-    li.textContent = `${Proudect.globArr[i].name} has this number of votes ${Proudect.globArr[i].votes} ${Proudect.globArr[i].shown}`;
+    li.textContent = `${Proudect.globArr[i].name} has this number of votes ${Proudect.globArr[i].votes} and has this number of shown ${Proudect.globArr[i].shown}`;
   }
   leftImageElement.removeEventListener('click', handleClick);
   middleImageElemnt.removeEventListener('click', handleClick);
   rightImageElement.removeEventListener('click', handleClick);
 }
 
-
-
 function generateRandomIndex() {
   return Math.floor(Math.random() * Proudect.globArr.length);
-
 }
-
